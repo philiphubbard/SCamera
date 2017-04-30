12e9abe70a95296c51cb0406707942f4a4f47f09
Philip Hubbard <philiphubbardsoftware@gmail.com>
2017-04-30 10:07:18 -0700

    Initial Commit

---
+++ SCamera/SCamera.h
@@ -0,0 +1,19 @@
+//
+//  SCamera.h
+//  SCamera
+//
+//  Created by Philip on 4/30/17.
+//  Copyright © 2017 Philip Hubbard. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for SCamera.
+FOUNDATION_EXPORT double SCameraVersionNumber;
+
+//! Project version string for SCamera.
+FOUNDATION_EXPORT const unsigned char SCameraVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SCamera/PublicHeader.h>
+
+
